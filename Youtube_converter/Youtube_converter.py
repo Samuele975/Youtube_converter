cf11fe332df1cdbbcb0fe6c6acd8f5c1149f7db4
Samuele975 <85025717+Samuele975@users.noreply.github.com>
2021-07-04 17:41:04 +0200

    Add files via upload

--- Youtube_converter/Youtube_converter.py
+++ Youtube_converter/Youtube_converter.py
@@ -3,10 +3,10 @@ import tkinter
 
 version = '1.1'             # this is version
 
-window = tkinter.Tk()                   # creation window
-window.geometry('600x400')              #
-window.title('Youtube converter mp3')   #
-window.resizable(width=0, height=0)     #
+window = tkinter.Tk()                           # creation window
+window.geometry('600x400')                      #
+window.title('Convertitore da Youtube a mp3')   #
+window.resizable(width=0, height=0)             #
 
 img = tkinter.PhotoImage(file='3.png')      # add background image
 bg = tkinter.Label(window, image=img)       #
@@ -20,8 +20,12 @@ version = tkinter.Label(window, text=f'v  {version}', bg='black')        # creat
 version.pack(side='right', anchor='s')                                   #
 
 
-def converter(url):                                   # function that request URL and convert and download the mp3 sound
-    audiod = youtube_dl.YoutubeDL({'format': 'bestaudio', 'postprocessors': [{ 'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192', }]})
+def converter(url):                                                 # function that request URL and convert and download the mp3 sound
+    audiod = youtube_dl.YoutubeDL({'format': 'bestaudio',
+                                   'postprocessors':
+                                       [{ 'key': 'FFmpegExtractAudio',
+                                         'preferredcodec': 'mp3',
+                                         'preferredquality': '192', }]})
 
     try:
         audiod.extract_info(url)
@@ -31,18 +35,18 @@ def converter(url):                                   # function that request UR
         text_download.title('Download')                                     #
 
         tdl = tkinter.Label(master=text_download,
-                            text='Video downloaded successfully', font=13)     # creation text download label
-        tdl.pack()                                                             #
-        tdl.place(x=20, y=50)                                                  #
+                            text='Video scaricato correttamente!', font=13)     # creation text download label
+        tdl.pack()                                                              #
+        tdl.place(x=20, y=50)                                                   #
 
     except:
         text_error = tkinter.Tk()                           # creation window that show label error
         text_error.geometry('200x100')                      #
-        text_error.title('ERROR')                           #
+        text_error.title('ERRORE')                          #
 
-        label_error = tkinter.Label(master=text_error, text='Invalid URL!', font=15, fg='red')    # creation label error
-        label_error.pack()                                                                        #
-        label_error.place(x=55, y=35)                                                             #
+        label_error = tkinter.Label(master=text_error, text='URL invalido!', font=15, fg='red')    # creation label error
+        label_error.pack()                                                                         #
+        label_error.place(x=55, y=35)                                                              #
 
 
 button = tkinter.Button(master=window, command=lambda: converter(box.get()))                       # creation button
@@ -50,7 +54,7 @@ button.config(text='Converti e scarica', fg='white', bg='black')                
 button.pack(padx=20)                                                                               #
 button.place(x=230, y=202)                                                                         #
 
-author = tkinter.Label(master=window, text='Created by  SV975', font=13, bg='black')  # creation author label
-author.pack(side='left', anchor='s')                                                             #
+author = tkinter.Label(master=window, text=' SV975', font=13, bg='black')           # creation author label
+author.pack(side='left', anchor='s')                                                #
 
 window.mainloop()
